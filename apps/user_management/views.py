5067e4f93826c5609858678a786d6f07b6a58c25
Sumit Kumar <sumit.kumar2@tigeranalytics.com>
2024-08-13 14:27:43 +0530

    docstring & Exception added

--- apps/user_management/views.py
+++ apps/user_management/views.py
@@ -1,5 +1,12 @@
+"""
+This module contains the view functions for handling HTTP requests and rendering
+responses for the application. It is responsible for processing user inputs,
+interacting with the models, and returning the appropriate templates or JSON data.
+
+"""
+
 import logging
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from .service import get_user_service
 
 logger = logging.getLogger(__name__)
@@ -16,6 +23,29 @@ async def get_users():
         user_lsit = get_user_service()
         logger.info("End of the view")
         return user_lsit
-    except Exception as e:
+    except ValueError as e:
         logger.error(e)
 
+
+@router.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    """
+    Retrieve an item by ID. If there is no optional
+    String Provided, In the return dict, It retruns the value as None
+
+    Args:
+        item_id (int): The ID of the item.
+        q (str, optional): An optional query string.
+
+    Returns:
+        dict: A dictionary containing the item's details like item id
+            & the optinal string
+    Raises:
+        HTTPException: If the item_id is invalid.
+        ValueError: If the `id` provided is not a positive integer.
+    """
+    logger.info("Start of the view")
+    if item_id == 0:
+        raise HTTPException(status_code=400, detail="Invalid ID")
+    logger.info("End of the view")
+    return {"item_id": item_id, "q": q}
