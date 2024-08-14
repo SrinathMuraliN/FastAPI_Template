7e7ba72248c1531da2212a0362f74b68c8ac4e44
Srinath Murali <91878714+SrinathMuraliN@users.noreply.github.com>
2024-08-15 00:13:48 +0530

    Adding Authentication Middleware

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import logging
 import os
 from contextlib import asynccontextmanager
 from typing import AsyncIterator
 
-from fastapi import FastAPI
+from aad_token_verify import get_verified_payload
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
 from starlette.middleware.sessions import SessionMiddleware
 
 from apps.user_management import views
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
@@ -37,6 +41,40 @@ def setup_app():
 
 
 app = setup_app()
+
+
+# @app.middleware("http")
+async def authentication_middleware(request: Request, call_next):
+    """
+    Function to add authentication middleware
+    """
+    authorization_header = request.headers.get("authorization", "")
+    if authorization_header.startswith("Bearer "):
+        token = authorization_header.split(" ")[1]
+        try:
+            token_verifier = get_verified_payload(
+                token,
+                tenant_id=os.environ.get("AZURE_TENANT_ID", None),
+                audience_uris=[os.environ.get("spclientid", None)],
+            )
+            try:
+                username = token_verifier.get("upn") or token_verifier.get(
+                    "unique_name"
+                )
+                user = User.objects.get(email=username)
+                if user:
+                    response = await call_next(request)
+                    return response
+            except User.DoesNotExist as exc:
+                raise HTTPException(status_code=404, detail="Authentication \
+                                    Error") from exc
+
+        except Exception as exc:
+            raise HTTPException(status_code=404, detail="Authentication \
+                                Error") from exc
+    else:
+        raise HTTPException(status_code=404, detail="No Bearer Token")
+
 
 # Including the router
 app.include_router(views.router, prefix="/api", tags=["Login"])
