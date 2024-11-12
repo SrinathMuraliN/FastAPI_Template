00c40953b63cb019457a9be3a88d33ac305c12ff
Srinath Murali <srinath.murali@tigeranalytics.com>
2024-11-12 12:47:52 +0530

    Update db_connection.py

--- utility/db_connection.py
+++ utility/db_connection.py
@@ -1,12 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from  sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./test.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Replace with below code for sqlserver
+# username = os.getenv("DB_USERNAME")
+# password = os.getenv("DB_PASSWORD")
+# server = os.getenv("DB_SERVER")
+# database = os.getenv("DB_NAME")
+# DATABASE_URL = f"mssql+aioodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(DATABASE_URL, pool_size=20)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine,class=AsyncSession)
 
 Base = declarative_base()
 
